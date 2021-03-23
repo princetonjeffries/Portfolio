7247f2a7de7a74e88e78225558bddc46b92694f4
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-03-23 16:49:25 -0700

    resume added and about me placeholder image added

--- scripts/script.js
+++ scripts/script.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
     $("#themeBtn").click(function(){
         
         // Change src attribute of image
-        if(i == 0){
-            $('body').css("background-image", "url(images/backgroundsunset.gif)");
-            i++;
-        } else if (i == 1){
+        if(i == 0){ //if i = 0 when button pressed, switch image to sunset
             $('body').css("background-image", "url(images/backgroundnight.gif)");
             i++;
-        } else if (i == 2){
+        } else if (i == 1){ // when i = 1 and button is pressed, switch to night
             $('body').css("background-image", "url(images/backgroundtest3.gif)");
+            i++;
+        } else if (i == 2){ //when i = 2 and button is pressed switch to beginning image and reset i.
+            $('body').css("background-image", "url(images/backgroundsunset.gif)");
             i = 0;
         }
     });    
