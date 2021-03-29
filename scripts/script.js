b65d69eefeccf01336d5af6999d1b32f5020247c
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-03-29 15:59:10 -0700

    background change

--- scripts/script.js
+++ scripts/script.js
@@ -6,10 +6,10 @@ $(function(){
         
         // Change src attribute of image
         if(i == 0){ //if i = 0 when button pressed, switch image to sunset
-            $('body').css("background-image", "url(images/backgroundnight.gif)");
+            $('body').css("background-color", "navy");
             i++;
         } else if (i == 1){ // when i = 1 and button is pressed, switch to night
-            $('body').css("background-image", "url(images/backgroundtest3.gif)");
+            $('body').css("background-color", "#2C7BD4");
             i++;
         } else if (i == 2){ //when i = 2 and button is pressed switch to beginning image and reset i.
             $('body').css("background-image", "url(images/backgroundsunset.gif)");
