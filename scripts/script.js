54126bb51fc8dc997b6cb7100703c21fbd86c080
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-04-14 16:04:08 -0700

    Added background images as well as spacing for the work page

--- scripts/script.js
+++ scripts/script.js
@@ -9,13 +9,14 @@ $(function(){
         
         // Change src attribute of image
         if(i == 0){ //if i = 0 when button pressed, switch image to sunset
-            $('body').css("background-color", "#5d646e");
+            //$('body').css("background-color", "#5d646e");
+            $('body').css("background-image", "url(images/mountains.jpg)");
             i++;
         } else if (i == 1){ // when i = 1 and button is pressed, switch to night
-            $('body').css("background-color", "#5e6e5d");
+            $('body').css("background-image", "url(images/space.jpg)");
             i++;
         } else if (i == 2){ //when i = 2 and button is pressed switch to beginning image and reset i.
-            $('body').css("background-color", "rgb(85, 55, 55)");
+            $('body').css("background-image", "url(images/citynight.jpg)");
             i = 0;
         }
     });    
