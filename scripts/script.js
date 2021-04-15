ea831c3147d98044f2e3fd32a0c902e4d7afde95
princetonjeffries <71167628+princetonjeffries@users.noreply.github.com>
2021-04-14 17:24:23 -0700

    color changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
 var i = 0;
 $(function(){
+    $('body').css("background-image", sessionStorage.getItem('background'));
     $('#web1').hover(function(){
         $('this').css('opacity', '10%');
     })
     $('.fronttext').fadeTo(1000, 1);
     $('.aboutme').fadeTo(500, 1);
     $("#themeBtn").click(function(){
-        
         // Change src attribute of image
         if(i == 0){ //if i = 0 when button pressed, switch image to sunset
             //$('body').css("background-color", "#5d646e");
             $('body').css("background-image", "url(images/mountains.jpg)");
             i++;
-        } else if (i == 1){ // when i = 1 and button is pressed, switch to night
+        } else if (i==1){ // when i = 1 and button is pressed, switch to night
             $('body').css("background-image", "url(images/space.jpg)");
             i++;
-        } else if (i == 2){ //when i = 2 and button is pressed switch to beginning image and reset i.
+        } else if (i==2){ //when i = 2 and button is pressed switch to beginning image and reset i.
             $('body').css("background-image", "url(images/citynight.jpg)");
             i = 0;
         }
